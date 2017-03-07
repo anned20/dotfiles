5de355770e0f2ecbb79a11a0e999a2f1f0ec938c
Anne Douwe Bouma <annedouwe@sibben.nl>
2017-03-07 16:34:16 +0100

    STUFFS

--- scripts/polybar.sh
+++ scripts/polybar.sh
@@ -5,7 +5,7 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 if [ $(hostname) = 'annedouwe-pc' ]; then
-	MONITOR=VGA-0 polybar -c $HOME/.polybar.conf bar &
+	MONITOR=VGA-1 polybar -c $HOME/.polybar.conf bar &
 	MONITOR=DVI-0 polybar -c $HOME/.polybar.conf bar &
 else
 	polybar -c $HOME/.polybar.conf bar &
