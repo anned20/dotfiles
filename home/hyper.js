3213a5110fb52b6bca16fafe4d717c0d9f7063c3
Anne Douwe Bouma <annedouwe@sibben.nl>
2017-03-22 14:07:43 +0100

    Termite is better

--- home/hyper.js
+++ home/hyper.js
@@ -19,12 +19,6 @@ module.exports = {
 		// set to true for blinking cursor
 		cursorBlink: false,
 
-		// color of the text
-		foregroundColor: '#fff',
-
-		// border color (window, tabs)
-		borderColor: '#292A2B',
-
 		// custom css to embed in the main window
 		css: '',
 
@@ -42,28 +36,6 @@ module.exports = {
 
 		// custom padding (css format, i.e.: `top right bottom left`)
 		padding: '0px 6px 16px 6px',
-
-		// the full list. if you're going to provide the full color palette,
-		// including the 6 x 6 color cubes and the grayscale map, just provide
-		// an array here instead of a color map object
-		colors: {
-			black: '#000000',
-			red: '#ff0000',
-			green: '#33ff00',
-			yellow: '#ffff00',
-			blue: '#0066ff',
-			magenta: '#cc00ff',
-			cyan: '#00ffff',
-			white: '#d0d0d0',
-			lightBlack: '#808080',
-			lightRed: '#ff0000',
-			lightGreen: '#33ff00',
-			lightYellow: '#ffff00',
-			lightBlue: '#0066ff',
-			lightMagenta: '#cc00ff',
-			lightCyan: '#00ffff',
-			lightWhite: '#ffffff'
-		},
 
 		// the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
 		// if left empty, your system's login shell will be used by default
@@ -104,7 +76,7 @@ module.exports = {
 	plugins: [
 		'hyperline',
 		'hyperlinks',
-		'hyper-darkmatter'
+		'hyper-darkmatter',
 	],
 
 	// in development, you can create a directory under
