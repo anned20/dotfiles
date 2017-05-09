2ccf082d6d92bf027a4198aa53ea316a2d1470e3
Anne Douwe <annedouwebouma@gmail.com>
2017-05-09 18:04:12 +0200

    Changes

--- install.sh
+++ install.sh
@@ -18,17 +18,16 @@ echo "${red}It is recommended to answer yes or y to all questions for everything
 echo "${red}You may also need to type 'exit' after you got into the zsh shell to continue the installation"
 read -p "${yellow}Press any key to continue..."
 
-echo "${yellow}Installing Hyper${green}"
+echo "${yellow}Installing termite${green}"
 
-mkdir -p ~/tmp/
-wget https://hyper-updates.now.sh/download/linux_deb -O ~/tmp/hyper.deb
-sudo dpkg -i ~/tmp/hyper.deb
+sudo apt-get install -y libgtk2.0-dev
+wget -O - https://raw.githubusercontent.com/Corwind/termite-install/master/termite-install.sh | bash
 
 echo "${yellow}Installing i3 deps and building${green}"
 
 sudo add-apt-repository -y ppa:tjormola/i3-unstable
 sudo apt-get update
-sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-xinerama0-dev libxcb-xrm-dev dunst libanyevent-i3-perl
+sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-xinerama0-dev libxcb-xrm-dev dunst libanyevent-i3-perl libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-x11-dev
 mkdir -p $HOME/tmp
 git clone https://github.com/Airblader/i3.git $HOME/tmp/i3-gaps
 cd $HOME/tmp/i3-gaps
