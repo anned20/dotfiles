4cd32e48890b08d32be61254502c384451452ad7
Anne Douwe <annedouwebouma@gmail.com>
2017-05-25 12:51:09 +0200

    New install script and spacemacs

--- install.sh
+++ install.sh
@@ -6,156 +6,160 @@ yellow=`tput setaf 3`
 reset=`tput sgr0`
 dotfiles="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Functions
 link() {
-	from="$1"
-	to="$2"
-	echo "Linking '$to' -> '$from'"
-	rm -f $to
-	ln -s "$from" "$to"
+    from="$1"
+    to="$2"
+    echo "Linking '$to' -> '$from'"
+    rm -f $to
+    ln -s "$from" "$to"
 }
 
-echo "${red}It is recommended to answer yes or y to all questions for everything to work properly!"
-echo "${red}You may also need to type 'exit' after you got into the zsh shell to continue the installation"
-read -p "${yellow}Press any key to continue..."
+command_exists () {
+    type "$1" > /dev/null 2>&1
+}
 
-echo "${yellow}Installing Hyper${green}"
+# Link all dotfiles from $dotfiles/home to the home folder
+linkfiles () {
+    echo "${yellow}Linking dotfiles${reset}"
 
-mkdir -p ~/tmp/
-wget https://hyper-updates.now.sh/download/linux_deb -O ~/tmp/hyper.deb
-sudo dpkg -i ~/tmp/hyper.deb
+    for location in $dotfiles/home/*; do
+        file="${location##*/}"
+        file="${file}"
+        link "$location" "$HOME/.$file"
+    done
 
-echo "${yellow}Installing i3 deps and building${green}"
+    link "$dotfiles/termiteconf/config" "~/.config/termite/config"
+}
 
-sudo add-apt-repository -y ppa:tjormola/i3-unstable
-sudo apt-get update
-sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-xinerama0-dev libxcb-xrm-dev dunst libanyevent-i3-perl
-mkdir -p $HOME/tmp
-git clone https://github.com/Airblader/i3.git $HOME/tmp/i3-gaps
-cd $HOME/tmp/i3-gaps
-autoreconf --force --install
-rm -rf build/
-mkdir -p build && cd build/
-../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-make
-sudo make install
-cd $dotfiles
+# Install termite if not already installed
+installtermite() {
+    if ! command_exists termite; then
+        echo "${yellow}Installing termite${green}"
 
-echo "${yellow}Installing polybar${green}"
-sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev
-git clone --recursive https://github.com/jaagr/polybar ~/tmp/polybar
-mkdir ~/tmp/polybar/build
-cd ~/tmp/polybar/build
-cmake ..
-sudo make install
-cd $dotfiles
+        sudo apt-get install -y libgtk2.0-dev
+        wget -P /tmp/ -O - https://raw.githubusercontent.com/Corwind/termite-install/master/termite-install.sh | bash
+    else
+        echo "${yellow}Termite is already installed${green}"
+    fi
 
-echo "${yellow}Installing i3 addons${green}"
+    if ! command_exists zsh; then
+        echo "${yellow}Installing ZSH${green}"
 
-sudo apt-get install -y i3status i3lock xautolock scrot imagemagick feh thunar gvfs compton python-gtk2-dev rofi
+        sudo apt-get install -y zsh
 
-echo "${yellow}Doing i3 stuff${green}"
+        echo "${yellow}Installing Oh-My-ZSH${green}"
+        sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-link "$dotfiles/i3/" "$HOME/.i3"
+        echo "${yellow}Installing custom Oh-My-ZSH plugins${green}"
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-echo "${yellow}Downloading and installing fonts${green}"
+        echo "${yellow}Changing shell to ZSH${green}"
+        sudo chsh -s $(which zsh)
+    else
+        echo "${yellow}ZSH is already installed${green}"
+    fi
+}
 
-mkdir -p $HOME/tmp
-wget https://www.fontsquirrel.com/fonts/download/roboto -P $HOME/tmp
-mv $HOME/tmp/roboto $HOME/tmp/roboto.zip
-sudo apt-get install -y unzip
-sudo unzip $HOME/tmp/roboto.zip -d /usr/share/fonts
-wget http://fontawesome.io/assets/font-awesome-4.7.0.zip -O ~/tmp/fontawesome.zip
-unzip ~/tmp/fontawesome.zip
-sudo cp font-awesome-4.7.0/fonts/fontawesome-webfont.ttf /usr/share/fonts/fontawesome.ttf
-sudo fc-cache -f -v
+# Install i3 if not already installed
+installi3() {
+    if ! command_exists i3; then
+        echo "${yellow}Installing i3 deps and building${green}"
 
-echo "${yellow}Making it pretty${green}"
+        sudo add-apt-repository -y ppa:tjormola/i3-unstable
+        sudo apt-get update
+        sudo apt-get install -y libxcb1-dev libxcb-keysyms1-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-xinerama0-dev libxcb-xrm-dev libanyevent-i3-perl
+        git clone https://github.com/Airblader/i3.git /tmp/i3-gaps
+        /tmp/i3-gaps
+        autoreconf --force --install
+        rm -rf build/
+        mkdir -p build && cd build/
+        ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+        make
+        sudo make install
 
-sudo apt-get install -y gtk2-engines-murrine gtk2-engines-pixbuf mate-terminal
-sudo sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' >> /etc/apt/sources.list.d/arc-theme.list"
-wget http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key
-sudo apt-key add - < Release.key
-sudo apt-get update && sudo apt-get install arc-theme
-sudo npm install -g i3-style
-wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install-papirus-home-gtk.sh | sh
-sudo cp $HOME/dotfiles/scripts/brightness /usr/bin/brightness
+        link "$dotfiles/i3/" "$HOME/.i3"
+    else
+        echo "${yellow}i3 is already installed${green}"
+    fi
+}
 
-echo "${yellow}Add this to your crontab by using crontab -e${green}"
-echo "*/15 * * * * ~/dotfiles/scripts/setbackground.sh"
-read -p "${yellow}Press any key to continue..."
+# Install spacemacs if not already installed
+installspacemacs() {
+    if [ -d ~/.emacs.d ]; then
+        read -r -p "${yellow}Emacs directory already exists. Delete .emacs.d and clone again?${red}[y/N] ${reset}" response
+        if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+        then
+            echo "${yellow}Removing .emacs.d${reset}"
+            rm -rf ~/.emacs.d
+            echo "${yellow}Cloning spacemacs${reset}"
+            git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+        else
+            echo "${yellow}Didn't remove .emacs.d${reset}"
+        fi
+    else
+        echo "${yellow}Cloning spacemacs${reset}"
+        git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+    fi
+}
 
-echo "${yellow}Music!${green}"
+# Install polybar if not already installed
+installpolybar() {
+    if ! command_exists polybar; then
+        echo "${yellow}Installing polybar${green}"
+        sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev
+        git clone --recursive https://github.com/jaagr/polybar /tmp/polybar
+        mkdir -p /tmp/polybar/build
+        cd /tmp/polybar/build
+        cmake ..
+        sudo make install
+    else
+        echo "${yellow}Polybar is already installed${green}"
+    fi
+}
 
-sudo apt-get install -y python-pip python3-pip ffmpeg cmus mpd ncmpcpp
-sudo pip3 install setuptools mps-youtube youtube_dl
-link "$dotfiles/mpd/" "$HOME/.mpd"
-link "$dotfiles/ncmpcpp/" "$HOME/.ncmpcpp"
+# Install fonts
+installfonts() {
+    git clone https://github.com/powerline/fonts.git /tmp/powerlinefonts
+    bash /tmp/powerlinefonts/install.sh
+    wget http://fontawesome.io/assets/font-awesome-4.7.0.zip -O /tmp/fontawesome.zip
+    unzip /tmp/fontawesome.zip -d /tmp/
+    sudo cp /tmp/font-awesome-4.7.0/fonts/fontawesome-webfont.ttf /usr/share/fonts/fontawesome.ttf
+    sudo fc-cache -f -v
+}
 
-echo "${yellow}Installing ZSH${green}"
+# Install theme
+installtheme() {
+    sudo apt-get install -y arc-theme lxappearance
 
-sudo apt-get install -y zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-sudo chsh -s $(which zsh)
+    wget -P /tmp/ -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-icon-theme/master/install-papirus-home-gtk.sh | sh
 
-echo "${yellow}Installing ZSH plugins${green}"
+    echo "${yellow}Add this to your crontab by using crontab -e${green}"
+    echo "*/15 * * * * ~/dotfiles/scripts/setbackground.sh"
+    read -p "${yellow}Press any key to continue..."
+}
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+# Misc things
+misc() {
+    echo "${yellow}Installing some addons and utilities${green}"
+    sudo apt-get install -y i3status i3lock xautolock scrot imagemagick feh thunar gvfs compton rofi unzip exuberant-ctags silversearcher-ag
 
-echo "${yellow}Linking dotfiles${green}"
+    echo "${yellow}Installing FZF${green}"
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+    ~/.fzf/install
+}
 
-for location in $dotfiles/home/*; do
-	file="${location##*/}"
-	file="${file}"
-	link "$location" "$HOME/.$file"
-done
-chmod +x $HOME/.lock.sh
-
-link "$dotfiles/ssh/config" "$HOME/.ssh/config"
-
-echo "${yellow}Doing vim stuff${green}"
-
-sudo add-apt-repository -y ppa:jonathonf/vim
-sudo apt-get update
-sudo apt-get install -y vim
-link "$dotfiles/vim/vimrc" "$HOME/.vimrc"
-link "$dotfiles/vim/" "$HOME/.vim"
-
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
-
-echo "${yellow}Installing vim plugins${green}"
-
-vim +PluginInstall +qall
-
-read -r -p "${yellow}Clean up vim plugins in .vim/bundle? ${red}[y/N] ${green}" response
-if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+# Actual program
+read -r -p "${yellow}Continue? Your files might be overwritten. ${red}[y/N] ${reset}" response
+if ! [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
 then
-	vim +PluginClean +qall
+    exit 1
+else
+    linkfiles
+    installtermite
+    installi3
+    installpolybar
+    installfonts
+    installtheme
+    misc
 fi
-
-echo "${yellow}Installing YouCompleteMe${green}"
-
-cd $dotfiles/vim/bundle/YouCompleteMe
-./install.py
-
-echo "${yellow}Installing CTags${green}"
-
-sudo apt-get install -y exuberant-ctags
-
-echo "${yellow}Installing the silver searcher (ag)${green}"
-
-sudo apt-get install -y silversearcher-ag
-
-echo "${yellow}Installing FZF${green}"
-
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
-
-echo "${yellow}Installing grip (to view MarkDown locally)${green}"
-
-sudo pip install grip
-
-echo "${yellow}Cleaning up${green}"
-
-rm -rf ~/tmp/fontawesome*
-
-echo "${yellow}Done!${reset}"
